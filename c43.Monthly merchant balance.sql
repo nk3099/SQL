ec9fc74d11309429f178e796b53dc635c9c52179
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2024-12-31 07:45:02 +0530

    complex_sql

---
+++ c43.Monthly merchant balance.sql
@@ -0,0 +1,22 @@
+--Day43: Monthly merchant balance:
+/*
+say you have access to all the transactions for a given merchant account. 
+Write a query to print cumulative balance of the merchant account at the end of each day, 
+with the total balance reset back to zero at the end of the month. 
+Output the transaction date and cumulative balance.
+*/
+
+
+with cte as 
+(select
+transaction_date::date as transaction_date,
+sum(case when type='withdrawal' then -1*amount else amount end) as amount
+group by transaction_date::date
+order by transaction_date::date
+)
+
+select
+transaction_date
+sum(amount) over (partition by extract(year from transaction_date),extract(month from transaction_date) 
+order by transaction_date) as cum_sum 
+from cte
