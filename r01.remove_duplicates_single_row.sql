f237a39982739f58ac4a7736782d08181706228e
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-01-10 21:51:53 +0530

    sql

---
+++ r01.remove_duplicates_single_row.sql
@@ -0,0 +1,51 @@
+--Rishasbh Mishra SQL
+--Remove_duplicates_single_row:
+
+create table travel (
+source varchar(10),
+destination varchar(10),
+distance varchar(10));
+
+insert into travel (source, destination, distance)
+values
+('Mumbai', 'Bangalore', 500),
+('Bangalore','Mumbai', 500),
+('Delhi', 'Mathura', 150),
+( 'Mathura','Delhi', 150),
+('Nagpur', 'Pune', 500),
+( 'Pune','Nagpur', 500);
+
+select * from travel;
+
+/*
+source     destination distance  
+---------- ----------- ----------
+Mumbai     Bangalore   500       
+Bangalore  Mumbai      500       
+Delhi      Mathura     150       
+Mathura    Delhi       150       
+Nagpur     Pune        500       
+Pune       Nagpur      500   
+
+OUTPUT:
+source     destination distance  
+---------- ----------- ----------
+Mumbai     Bangalore   500       
+Delhi      Mathura     150       
+Pune       Nagpur      500    
+*/
+
+
+--Method: Using SELF Join
+
+with cte as
+(
+select *
+, row_number() over(order by (select null)) as slno
+from travel
+)
+
+select c1.source, c1.destination, c1.distance
+from cte c1
+join cte c2 on c1.source=c2.destination and c1.slno < c2.slno
+
