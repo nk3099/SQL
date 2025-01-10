4494bfdc7f11ab48c4cc0a21aac301091b870635
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-01-10 22:16:43 +0530

    sql

--- r01.remove_duplicates_single_row.sql
+++ r01.remove_duplicates_single_row.sql
@@ -36,7 +36,7 @@ Pune       Nagpur      500
 */
 
 
---Method: Using SELF Join
+--Method1: Using SELF Join
 
 with cte as
 (
@@ -48,4 +48,27 @@ from travel
 select c1.source, c1.destination, c1.distance
 from cte c1
 join cte c2 on c1.source=c2.destination and c1.slno < c2.slno
+
+--Method2: Using SUBQUERY
+
+select  *
+from travel t1 
+where not exists ( select *
+                   from travel t2 
+                   where t1.source=t2.destination
+                   and t1.destination=t2.source
+                   and t1.destination > t2.destination --acting as a true/false in subquery
+                   )
+
+--Method3: Using GREATEST & LEAST function
+
+select greatest(source,destination) as source, least(source,destination) as destination, max(distance) as distance 
+from travel 
+group by greatest(source,destination), least(source,destination)
+
+
+
+--example:
+select greatest(2,11,9,4); --outputs--: 11
+select least(2,11,9,4); --outputs--: 2
 
