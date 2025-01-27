75c5420e6d04410c8a43d638a3d356fd45fc3771
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-01-27 09:39:29 +0530

    complex_sql

--- c70.consecutive_seats.sql
+++ c70.consecutive_seats.sql
@@ -1,4 +1,4 @@
---day6Several friends at a cinema ticket office would like to reserve consecutive available seats. 
+--day70. Several friends at a cinema ticket office would like to reserve consecutive available seats. 
 --Can you help to query all the consecutive available seats order by the seat_id using the following cinema table?
 
 CREATE TABLE cinema (
@@ -31,5 +31,43 @@ select * from cinema;
 --3,4,5,7,8,14,15,17,18,19,20 --> 11 seats
 
 --method 1:
+with cte as (
 select *
+,row_number() over(order by seat_id) as rn
+,seat_id-row_number() over(order by seat_id) as grp
 from cinema
+where free=1
+)
+select * from
+(
+select *
+,count(*) over (partition by grp ) as cnt
+from cte
+) A 
+where cnt>1;
+
+--method 2:
+with cte as (
+select c1.seat_id as s1, c2.seat_id as s2
+from cinema c1
+inner join
+cinema c2
+on c1.seat_id+1=c2.seat_id
+where c1.free=1 and c2.free=1
+)
+select s1 from cte
+union --to remove duplicates
+select s2 from cte
+
+
+--method 3:
+select * 
+from 
+(
+select *
+,lag(free,1) over (order by seat_id) as prev_free
+,lead(free,1) over (order by seat_id) as next_free
+from 
+cinema
+)A
+where free=1 and (prev_free=1 or next_free=1)
