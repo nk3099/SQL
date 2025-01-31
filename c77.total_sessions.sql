98e3cbfe0021414b9f60614625813b4cc1bb0804
Neeraj Kumar <40226879+nk3099@users.noreply.github.com>
2025-01-31 15:51:40 +0530

    complex_sql

--- c77.total_sessions.sql
+++ c77.total_sessions.sql
@@ -21,3 +21,28 @@ insert into events VALUES (2, 'click', '2023-09-10 10:30:00');
 select * from events;
 
 --we need to find time difference between event_time of consecutive events.
+
+with cte as 
+(
+select *
+, lag(event_time,1,event_time) over (partition by userid order by event_time) as prev_event
+, datediff(minute,lag(event_time,1,event_time) over (partition by userid order by event_time), event_time) as time_diff
+--, case when datediff(minute,event_time,lag(event_time) over (partition by userid order by event_time))=30 then 1 else 0 end as inter
+from events
+)
+,cte2 as 
+(
+select *
+, case when time_diff<=30 then 0 else 1 end as flag
+, sum(case when time_diff<=30 then 0 else 1 end) over (partition by userid order by event_time) as session_grp
+from cte
+)
+
+select userid,session_grp+1 as session_id
+, min(event_time) as session_start_time
+, max(event_time) as session_end_time
+, count(*) as no_of_events
+, datediff(minute,min(event_time),max(event_time))as session_duration
+from cte2
+group by userid,session_grp
+order by userid,session_grp
